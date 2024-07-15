88a0c34e51b3eb18704f15ee5fafb7c57a2ed9cc
AlexJaegerPena <166617483+AlexJaegerPena@users.noreply.github.com>
2024-07-15 16:14:10 +0200

    added contact and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0F2480', // Navy Blue
-        secondary: '#3554D1', // lighter blue
-        accent: '#f97616' // Orange
+        primary: "#0F2480", // Navy Blue
+        secondary: "#3554D1", // lighter blue
+        accent: "#f97616", // Orange
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      }
+        sans: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [daisyui],
-}
+};
